590de18b8c3fefc6d1e384cfac6d7cc9e40e7d7f
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-04-07 13:37:06 -0600

    Add JSDOM. Add tests for getURLs function

--- src/crawl.js
+++ src/crawl.js
@@ -1,18 +1,30 @@
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
 function normalizeURL(url){
-  result = ""
-  parsedURL = new URL(url)
+  result = "";
+  parsedURL = new URL(url);
   
   if (parsedURL !== null) {
-    result = parsedURL.host + parsedURL.pathname
+    result = parsedURL.host + parsedURL.pathname;
   }
 
   if (result[result.length - 1] === '/'){
-    newStr = result.slice(0, result.length - 1)
-    result = newStr
+    newStr = result.slice(0, result.length - 1);
+    result = newStr;
   }
 
 
-  return result
+  return result;
 }
 
-module.exports = normalizeURL;
+function getURLsFromHTML(htmlStr, baseURL){
+  const dom = new JSDOM(htmlStr);
+  const arrURLs = dom.window.document.querySelectorAll('a');
+
+}
+
+module.exports = { 
+  normalizeURL,
+  getURLsFromHTML
+};
--- tests/crawl.test.js
+++ tests/crawl.test.js
@@ -1,21 +1,69 @@
-const normalizeURL = require('../src/crawl');
+const { normalizeURL, getURLsFromHTML } = require("../src/crawl");
 
-test('normalizes hostname', () => {
-  expect(normalizeURL('http://www.example.com/')).toBe('www.example.com')
+// Tests for normalizeURL()
+test("normalizes hostname", () => {
+  expect(normalizeURL("http://www.example.com/")).toBe("www.example.com");
 });
 
-test('normalize hostname and path', () => {
-  expect(normalizeURL('http://www.example.com/path/')).toBe('www.example.com/path')
+test("normalize hostname and path", () => {
+  expect(normalizeURL("http://www.example.com/path/")).toBe(
+    "www.example.com/path",
+  );
 });
 
-test('normalize hostname and path', () => {
-  expect(normalizeURL('http://www.example.com/path/to/file/')).toBe('www.example.com/path/to/file')
+test("normalize hostname and path", () => {
+  expect(normalizeURL("http://www.example.com/path/to/file/")).toBe(
+    "www.example.com/path/to/file",
+  );
 });
 
-test('normalize port', () => {
-  expect(normalizeURL('http://www.example.com/path/to/file/')).toBe('www.example.com/path/to/file')
+test("normalize port", () => {
+  expect(normalizeURL("http://www.example.com/path/to/file/")).toBe(
+    "www.example.com/path/to/file",
+  );
 });
 
-test('ignore port', () => {
-  expect(normalizeURL('http://www.example.com:80/path/to/file/')).toBe('www.example.com/path/to/file')
+test("ignore port", () => {
+  expect(normalizeURL("http://www.example.com:80/path/to/file/")).toBe(
+    "www.example.com/path/to/file",
+  );
+});
+
+// Tests for getURLsFromHTML()
+const baseURL = 'https://example.com'
+
+test("convert relative URL to absolute", () => {
+  expect(
+    getURLsFromHTML(
+      '<a href="/relative/path/">Relative</a>',
+      baseURL,
+    ),
+  ).toBe(["http://example.com/relative/path/"]);
+});
+
+test("return array of URL strings", () => {
+  expect(
+    getURLsFromHTML(
+      '<a href="http://external.com">External</a><a href="/internal/">Internal</a>',
+      baseURL,
+    ),
+  ).toBe(["http://external.com", "http://example.com/internal/"]);
+});
+
+test("should handle protocol-relative URLs", () => {
+  expect(
+    getURLsFromHTML(
+      '<a href="//protocol-relative.com">Protocol Relative</a>',
+      baseURL,
+    ),
+  ).toBe(["http://protocol-relative.com"]);
+});
+
+test("should ignore HTML string without URLs", () => {
+  expect(
+    getURLsFromHTML(
+      '<div>Sample text</div>',
+      baseURL,
+    ),
+  ).toBe([]);
 });
